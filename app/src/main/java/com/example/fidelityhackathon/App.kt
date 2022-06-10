2907e66107d66523012dc0fead0428a75e250dbe
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 20:02:12 +0530

    Added retrofit

---
+++ app/src/main/java/com/example/fidelityhackathon/App.kt
@@ -0,0 +1,8 @@
+package com.example.fidelityhackathon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+}
--- app/src/main/java/com/example/fidelityhackathon/MainActivity.kt
+++ app/src/main/java/com/example/fidelityhackathon/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.fidelityhackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
