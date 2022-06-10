dd65e748088887e0cf7da54717acd2022379f8a0
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 19:23:36 +0530

    Added hilt dependencies

--- app/src/main/java/com/example/fidelityhackathon/MainActivity.kt
+++ app/src/main/java/com/example/fidelityhackathon/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.fidelityhackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
